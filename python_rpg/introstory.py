4eb57431dc688d1d63d3113ae262bd048b9e875b
DostD <dostd956@hsd.k12.or.us>
2023-05-10 15:10:07 -0700

    improved introstory

---
+++ python_rpg/introstory.py
@@ -0,0 +1,39 @@
+introduction = (
+    "\n\nIn the vast reaches of the galaxy, humanity's quest for exploration led them to" + 
+    "\na remarkable discovery—a colossal abandoned spaceship of unknown origin," +
+    "\ndubbed the Aegis. Rumored to hold untold secrets and advanced technologies," +
+    "\nthe Aegis became the focal point of scientific intrigue and interstellar fascination.",
+    "\n\nEager to unlock the mysteries within, a team of intrepid explorers and scientists" + 
+    "\nembarked on a mission to board the enigmatic vessel. Led by Dr. Amelia Blackwood," + 
+    "\na brilliant astrophysicist, the crew set foot on the Aegis, unaware of the" + 
+    "\nimminent danger that awaited them.",
+    "\n\nAs they cautiously navigated the alien corridors, their awe quickly turned to" + 
+    "\nhorror. The Aegis had not been abandoned; it had been invaded. Sinister" + 
+    "\nextraterrestrial beings, known as the Dra'Kesh, had made this ghostly behemoth" + 
+    "\ntheir nest, infesting it with their malevolent presence.",
+    "\n\nThe once-gleaming spaceship now lay in ruin, with flickering lights casting" + 
+    "\neerie shadows on broken panels and twisted metal. Alien flora, with thorny tendrils and" + 
+    "\nluminescent hues, had taken root and begun to dominate the ship's decaying interior." + 
+    "\nThe air hummed with an otherworldly energy, the echo of an alien invasion.",
+    "\n\nSeparated from the rest of the crew, you find yourself isolated, your survival now" + 
+    "\ndependent on your wits and resourcefulness. Your objective is clear—to reach the" + 
+    "\nescape pod located in the heart of the ship. It is your only chance to evade the" + 
+    "\nclutches of the Dra'Kesh and make your way back to safety.",
+    "\n\nAs you make your way through the treacherous labyrinth of the Aegis, you will" + 
+    "\nencounter unimaginable horrors and face countless obstacles. The Dra'Kesh, with their" + 
+    "\nvicious intellect and deadly abilities, will stop at nothing to prevent your escape.",
+    "\n\nArmed with a meager selection of weapons salvaged from fallen comrades and your own" + 
+    "\ndetermination, you must gather your courage and press forward. The fate of the" + 
+    "\ncrew and perhaps even the future of humanity rests on your ability to outsmart" + 
+    "\nand outmaneuver the Dra'Kesh, navigating the twisted corridors and battling through" + 
+    "\ntheir malevolent forces.",
+    "\n\nWith each step closer to the escape pod, the stakes grow higher. Time is of the" + 
+    "\nessence, and the Aegis is crumbling around you. Will you overcome the" + 
+    "\nextraterrestrial threat and reach salvation, or will the Dra'Kesh claim" + 
+    "\nanother victim in their dark conquest of the stars?",
+    "\n\nEmbark on this gripping RPG adventure, where survival, cunning, and bravery" + 
+    "\nare your greatest allies. The fate of your crewmates, the secrets of the Aegis, and" + 
+    "\nthe possibility of escaping this alien-infested nightmare lie within your grasp." + 
+    "\nOnly you can ensure that the horrors lurking within the spaceship do not become your" + 
+    "\nfinal resting place in the cold void of space."
+    "\n")
